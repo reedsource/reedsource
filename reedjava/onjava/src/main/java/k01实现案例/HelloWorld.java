4340f4ba3d6078b008731b99bee42add6fea1a36
reedsource <reedsource@189.cn>
2022-05-06 23:26:52 +0800

    添加 onjava k01实现案例

---
+++ reedjava/onjava/src/main/java/k01实现案例/HelloWorld.java
@@ -0,0 +1,29 @@
+/*
+ * FileName: HelloWorld
+ * Author:   reedbook
+ */
+package k01实现案例;
+
+import top.ireed.deal.DealLog;
+
+/**
+ * 功能简述:
+ * 〈基本java程序〉
+ *
+ * @author reedbook
+ * @version 1.0.0
+ * date 2022/5/6 22:52
+ * reedsource@189.cn
+ */
+public class HelloWorld {
+	/**
+	 * 它将打印字符串 Hello World
+	 *
+	 * @param args 初始数据组
+	 */
+	public static void main(String[] args) {
+		// 打印 Hello World
+        //本处实际为标准打印 符合sonar规范 修改为调用工具类
+		DealLog.log("Hello World");
+	}
+}
---
+++ reedjava/onjava/src/main/java/k01实现案例/JavaMain.java
@@ -0,0 +1,29 @@
+/*
+ * FileName: JavaMain
+ * Author:   reedbook
+ */
+package k01实现案例;
+
+import top.ireed.deal.DealLog;
+
+/**
+ * 功能简述:
+ * 〈java启动main方法时附带数据   使用控制台java 运行时 附带数据进入主main方法〉
+ *
+ * @author reedbook
+ * @version 1.0.0
+ * date 2022/5/6 22:52
+ * reedsource@189.cn
+ */
+public class JavaMain {
+	public static void main(String[] args) {
+		if (args.length != 2) {
+			DealLog.log("用法: java javaMain <host1 msg> <host2 msg>");
+			System.exit(1);
+		}
+		String host1 = args[0];
+		String host2 = args[1];
+		DealLog.log(host1);
+		DealLog.log(host2);
+	}
+}
