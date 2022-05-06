72670aafed4ffaa2b5f9f61ba43bbd7c48cfb275
reedsource <reedsource@189.cn>
2022-05-06 23:27:41 +0800

    添加 onjava k03对象

---
+++ reedjava/onjava/src/main/java/k03对象/Dog.java
@@ -0,0 +1,48 @@
+/*
+ * FileName: Dog
+ * Author:   reedbook
+ */
+package k03对象;
+
+import top.ireed.deal.DealLog;
+
+/**
+ * 功能简述:
+ * 〈小狗〉
+ *
+ * @author reedbook
+ * @version 1.0.0
+ * date 2022/5/6 22:56
+ * reedsource@189.cn
+ */
+public class Dog {
+
+	int dogAge;
+
+	/*通过重写get set方法使类的属性暴露出来*/
+	public int getDogAge() {
+		return dogAge;
+	}
+
+	public void setDogAge(int dogAge) {
+		this.dogAge = dogAge;
+	}
+
+	//Java编译器将会为该类提供一个默认构造方法
+	public Dog(){
+	}
+
+	/*通过构造方法,使外部通过构造方法直接生成实体类*/
+	public Dog(String name){
+		//这个构造器仅有一个参数：name
+		DealLog.log("小狗的名字是 : " + name );
+	}
+}
+/*
+一个源文件中只能有一个public类
+一个源文件可以有多个非public类
+源文件的名称应该和public类的类名保持一致。例如：源文件中public类的类名是Employee，那么源文件应该命名为Employee.java。
+如果一个类定义在某个包中，那么package语句应该在源文件的首行。
+如果源文件包含import语句，那么应该放在package语句和类定义之间。如果没有package语句，那么import语句应该在源文件中最前面。
+import语句和package语句对源文件中定义的所有类都有效。在同一源文件中，不能给不同的类不同的包声明。
+*/
---
+++ reedjava/onjava/src/main/java/k03对象/K03.java
@@ -0,0 +1,38 @@
+/*
+ * FileName: K8001Test
+ * Author:   reedbook
+ */
+package k03对象;
+
+import top.ireed.deal.DealLog;
+
+/**
+ * 功能简述:
+ * 〈实现对象实体创建调用〉
+ *
+ * @author reedbook
+ * @version 1.0.0
+ * date 2022/5/6 22:56
+ * reedsource@189.cn
+ */
+public class K03 {
+	public static void main(String[] args) {
+		// 下面的语句将创建一个对象
+		Dog dog = new Dog();
+
+		DealLog.log(dog);
+		//未重写toString()方法时 将会直接打印类地址
+		//k03对象.Dog@223191a6
+
+		//使用构造函数创建有初始值的对象
+		dog = new Dog("tommy");
+		/* 通过方法来设定age */
+		dog.setDogAge(11);
+		/* 调用另一个方法获取age */
+		int a = dog.getDogAge();
+		DealLog.log(a);
+		/*你也可以像下面这样访问成员变量 */
+		//也可以
+		DealLog.log("变量值 : ", dog.getDogAge());
+	}
+}
