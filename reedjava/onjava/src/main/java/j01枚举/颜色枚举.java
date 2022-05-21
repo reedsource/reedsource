54c1d5dd3f0f98e0c4b0df008129c81c3b98e5e1
reedsource <reedsource@189.cn>
2022-05-21 23:58:12 +0800

    更新 onjava k01枚举

---
+++ reedjava/onjava/src/main/java/j01枚举/颜色枚举.java
@@ -0,0 +1,16 @@
+package j01枚举;
+
+/**
+ * 功能简述:
+ * 〈〉
+ *
+ * @author reedbook
+ * date 2022/5/21 23:39
+ * mail reedsource@189.cn
+ */
+public enum 颜色枚举 {
+	/**
+	 * RED 红色
+	 */
+	RED, GREEN, BLANK, YELLOW
+}
