d9e17e49efe7b155ea022bd3d59443518aaaed4e
reedsource <reedsource@189.cn>
2022-06-16 20:56:59 +0800

    更新 onjava k03对象.包装类.Integer包装类

--- reedjava/onjava/src/main/java/k03对象/包装类/Integer包装类/javaInteger.java
+++ reedjava/onjava/src/main/java/k03对象/包装类/Integer包装类/javaInteger.java
@@ -4,6 +4,7 @@
  */
 package k03对象.包装类.Integer包装类;
 
+import org.junit.Test;
 import top.ireed.deal.DealLog;
 
 /**
@@ -20,13 +21,18 @@ import top.ireed.deal.DealLog;
  * 为了解决这个问题，Java 语言为每一个内置数据类型提供了对应的包装类
  * 这种由编译器特别支持的包装称为装箱，所以当内置数据类型被当作对象使用的时候，编译器会把内置类型装箱为包装类。
  * 相似的，编译器也可以把一个对象拆箱为内置类型。Number 类属于 java.lang 包
+ *
+ * 包装类都建议使用.valueOf()方式创建
  */
 public class javaInteger {
-	public static void main(String[] args) {
-		Integer x = 5;
-		x = x + 10;
-		DealLog.log(x);
-	}
+	@Test
+	public void javaInteger_Test() {
+		//创建方法1 java9后移除
+		Integer x = new Integer("20");
+		//创建方式2 比方式1高效  其他包装类也推荐这种方式
+		Integer y = Integer.valueOf("20");
 
-	//15
+		DealLog.log(x, y);
+		//20 20
+	}
 }
