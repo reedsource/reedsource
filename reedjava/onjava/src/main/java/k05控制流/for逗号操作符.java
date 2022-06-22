c340968d7915ae2ec9d36678363e6a9e339c6870
reedsource <reedsource@189.cn>
2022-06-22 11:49:10 +0800

    更新 onjava k05控制流 for逗号操作符

---
+++ reedjava/onjava/src/main/java/k05控制流/for逗号操作符.java
@@ -0,0 +1,27 @@
+/*
+ * Author:   reedsource
+ */
+package k05控制流;
+
+import org.junit.Test;
+
+/**
+ * 功能简述:
+ * 〈〉
+ *
+ * @author reedsource
+ * @version 1.0.0
+ * date 2022/6/22 11:03
+ * reedsource@189.cn
+ */
+public class for逗号操作符 {
+
+	@Test
+	public void for逗号操作符_Test() {
+		// 是操作符
+		//初始化时可以定义多个相同类型的变量
+		for (int i = 1, j = i + 10; i < 5; i++, j = i * 2) {
+			System.out.println("i = " + i + " j = " + j);
+		}
+	}
+}
