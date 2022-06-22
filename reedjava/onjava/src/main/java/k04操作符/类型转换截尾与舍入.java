f83240d5ae5fadc2d09eb22c8f19558568a7f1a4
reedsource <reedsource@189.cn>
2022-06-22 10:19:57 +0800

    更新 onjava k04操作符 类型转换操作符

---
+++ reedjava/onjava/src/main/java/k04操作符/类型转换截尾与舍入.java
@@ -0,0 +1,65 @@
+/*
+ * Author:   reedsource
+ */
+package k04操作符;
+
+import org.junit.Test;
+import top.ireed.deal.DealLog;
+
+/**
+ * 功能简述:
+ * 〈窄化转型时需要注意截尾和舍入问题〉
+ *
+ * @author reedsource
+ * @version 1.0.0
+ * date 2022/6/22 9:57
+ * reedsource@189.cn
+ */
+public class 类型转换截尾与舍入 {
+
+	@Test
+	public void 类型转换截尾与舍入_Test() {
+		//double和float转型为整数类型时,默认总会对数值截尾
+		double a = 0.7, b = 0.4;
+		float f1 = 0.7f, f2 = 0.4f;
+		DealLog.log("(int)a: " + (int) a);
+		DealLog.log("(int)b: " + (int) b);
+		DealLog.log("(int)f1: " + (int) f1);
+		DealLog.log("(int)f2: " + (int) f2);
+		//(int)a: 0
+		//(int)b: 0
+		//(int)f1: 0
+		//(int)f2: 0
+
+		//对数据结果进行舍入
+		DealLog.log("Math.round(a): " + Math.round(a));
+		DealLog.log("Math.round(b): " + Math.round(b));
+		DealLog.log("Math.round(f1): " + Math.round(f1));
+		DealLog.log("Math.round(f2): " + Math.round(f2));
+		//Math.round(a): 1
+		//Math.round(b): 0
+		//Math.round(f1): 1
+		//Math.round(f2): 0
+
+		//注意 Math.round 在负数的时候 -1.5的时是-1  倒数0.9 0.8 0.7 0.6 0.5是第五个
+		DealLog.log("Math.round(-0.5): " + Math.round(-1.5));
+
+		//Math.ceil() 向上取整
+		DealLog.log("Math.ceil(1.1: " + Math.ceil(1.1));
+		DealLog.log("Math.ceil(1.5: " + Math.ceil(1.5));
+		DealLog.log("Math.ceil(1.6): " + Math.ceil(1.6));
+		DealLog.log("Math.ceil(-1.1): " + Math.ceil(-1.1));
+		DealLog.log("Math.ceil(-1.5): " + Math.ceil(-1.5));
+		DealLog.log("Math.ceil(-1.6): " + Math.ceil(-1.6));
+		//Math.ceil(1.1: 2.0
+		//Math.ceil(1.5: 2.0
+		//Math.ceil(1.6): 2.0
+		//Math.ceil(-1.1): -1.0
+		//Math.ceil(-1.5): -1.0
+		//Math.ceil(-1.6): -1.0
+
+
+	}
+
+
+}
--- reedjava/onjava/src/main/java/k04操作符/类型转换操作符.java
+++ reedjava/onjava/src/main/java/k04操作符/类型转换操作符.java
@@ -1,9 +1,10 @@
 /*
- * FileName: JavaCoerce
+ * FileName: 类型转换操作符
  * Author:   reedsource
  */
-package k03对象.强制类型转换;
+package k04操作符;
 
+import org.junit.Test;
 import top.ireed.deal.DealLog;
 
 /**
@@ -15,8 +16,10 @@ import top.ireed.deal.DealLog;
  * date 2022/5/7 14:06
  * reedsource@189.cn
  */
-public class JavaCoerce {
-	public static void main(String[] args) {
+public class 类型转换操作符 {
+
+	@Test
+	public void 类型转换操作符_Test() {
 		int i1 = 123;
 		byte b = (byte) i1;//强制类型转换为byte
 		DealLog.log("int强制类型转换为byte后的值等于" + b);
@@ -27,5 +30,13 @@ public class JavaCoerce {
 	 * 2. 格式：(type)value type是要强制类型转换后的数据类型
 	 * 整数的默认类型是 int
 	 * 浮点型不存在这种情况，因为在定义 float 类型时必须在数字后面跟上 F 或者 f
+	 *
+	 * 窄化转换 会造成信息丢失 编译器将会提示需要显式转换
+	 * 宽化转换 不需要显式转换 因为不会丢失信息
+	 *
+	 * boolean不允许任何类型的转换
+	 *
+	 * 对象可以在他的类型所在的族群中进行类型转换
+	 *
 	 * */
 }
