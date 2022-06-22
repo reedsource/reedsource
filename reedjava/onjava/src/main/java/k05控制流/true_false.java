ba2bcb47b02fa1d8f3aff1f6cb178c2019e1ec3e
reedsource <reedsource@189.cn>
2022-06-22 11:49:59 +0800

    更新 onjava k05控制流 true_false

---
+++ reedjava/onjava/src/main/java/k05控制流/true_false.java
@@ -0,0 +1,30 @@
+/*
+ * Author:   reedsource
+ */
+package k05控制流;
+
+import org.junit.Test;
+import top.ireed.deal.DealLog;
+
+/**
+ * 功能简述:
+ * 〈〉
+ *
+ * @author reedsource
+ * @version 1.0.0
+ * date 2022/6/22 10:48
+ * reedsource@189.cn
+ */
+public class true_false {
+
+	@Test
+	public void true_false_Test() {
+		// 真是非0值 假是0值
+		//java中不允许直接用数字来代替
+		DealLog.log(1 == 1);
+		DealLog.log(1 == 2);
+
+		//true
+		//false
+	}
+}
