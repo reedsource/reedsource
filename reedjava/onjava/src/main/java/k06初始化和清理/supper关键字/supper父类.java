00b5e56fc12da58522e8c77ad765da7885108ef3
reedsource <reedsource@189.cn>
2022-07-04 14:39:01 +0800

    更新 onjava k06初始化和清理.supper关键字

---
+++ reedjava/onjava/src/main/java/k06初始化和清理/supper关键字/supper父类.java
@@ -0,0 +1,14 @@
+/*
+ * Author:   reedsource
+ */
+package k06初始化和清理.supper关键字;
+
+import top.ireed.deal.DealLog;
+
+public class supper父类 {
+	String msg = "父类信息msg";
+
+	public void method() {
+		DealLog.log("父类方法method");
+	}
+}
