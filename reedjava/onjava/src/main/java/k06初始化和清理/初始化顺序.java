18b25df3f9dd5d4db4d3f0427b6237ae29a32613
ireedtop <ireedtop@outlook.com>
2022-07-05 21:45:40 +0800

    添加 k06初始化和清理 初始化顺序

---
+++ reedjava/onjava/src/main/java/k06初始化和清理/初始化顺序.java
@@ -0,0 +1,48 @@
+/*
+ * Author:   reedsource
+ */
+package k06初始化和清理;
+
+import org.junit.Test;
+import top.ireed.deal.DealLog;
+
+/**
+ * 功能简述:
+ * 〈构造器调用时的执行顺序〉
+ *
+ * @author reedsource
+ * @version 1.0.0
+ * date 2022/7/5 21:11
+ * reedsource@189.cn
+ */
+public class 初始化顺序 {
+	@Test
+	public void 初始化顺序_Test() {
+		House h = new House();
+		h.f();
+	}
+}
+
+class Window {
+	Window(String marker) {
+		DealLog.log("Window", marker);
+	}
+}
+
+class House {
+	Window w1 = new Window("在构造器前"); // Before constructor
+
+	House() {
+		DealLog.log("构造器进入");
+		w3 = new Window("构造器调用1");
+	}
+
+	Window w2 = new Window("构造器调用2");
+
+	void f() {
+		DealLog.log("f()");
+	}
+
+	//尾部
+	Window w3 = new Window("在构造器尾"); // At end
+}
