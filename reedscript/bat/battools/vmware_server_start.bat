7c0aa508017d82027521cb13f19a0092ade7a6d7
reedsource <reedsource@189.cn>
2022-08-28 22:42:09 +0800

    添加 wmware虚拟机启动停止脚本

---
+++ reedscript/bat/battools/vmware_server_start.bat
@@ -0,0 +1,4 @@
+@echo off
+"C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe" -T ws start "E:\ReedGO-S\ReedGO.vmx" nogui
+"C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe" -T ws start "E:\ReedGO-K\ReedGO.vmx" nogui
+"C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe" -T ws start "E:\ReedGO-G\ReedGO.vmx" nogui
---
+++ reedscript/bat/battools/vmware_server_stop.bat
@@ -0,0 +1,4 @@
+@echo off
+"C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe" stop "E:\ReedGO-S\ReedGO.vmx" nogui
+"C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe" stop "E:\ReedGO-K\ReedGO.vmx" nogui
+"C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe" stop "E:\ReedGO-G\ReedGO.vmx" nogui
