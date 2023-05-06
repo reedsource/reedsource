7f8b3e2933487e13cbba91b9cde85da537c07eec
reedsource <reedsource@189.cn>
2023-05-06 15:02:04 +0800

    更新 IO工具类 添加文件转字符串,字符串转文件方法

--- reedtools/src/main/java/top/ireed/deal/DealIo.java
+++ reedtools/src/main/java/top/ireed/deal/DealIo.java
@@ -10,6 +10,7 @@ import top.ireed.general.TopException;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.Base64;
 
 /**
  * 功能简述:
@@ -21,60 +22,117 @@ import java.nio.file.Files;
  * reedsource@189.cn
  */
 public class DealIo {
-	/**
-	 * 将字符串写入到文件中
-	 * <p>
-	 * 目录及文件不存在将尝试创建,注意文件存在时,内容将会被覆盖
-	 *
-	 * @param file 生成文件的路径
-	 * @param str  文件的字符串
-	 */
-	public static void toFileIo(File file, String str) throws TopException {
-		try {
-			//文件不存在  且  创建文件父级目录成功 创建文件成功
-			if (!file.exists() && (!DealContents.newContents(file) || !file.createNewFile())) {
-				DealLog.log(file, "写入到文件创建失败");
-				return;
-			}
+    /**
+     * 将字符串写入到文件中
+     * <p>
+     * 目录及文件不存在将尝试创建,注意文件存在时,内容将会被覆盖
+     *
+     * @param file 生成文件的路径
+     * @param str  文件的字符串
+     */
+    public static void toFileIo(File file, String str) throws TopException {
+        try {
+            //文件不存在  且  创建文件父级目录成功 创建文件成功
+            if (!file.exists() && (!DealContents.newContents(file) || !file.createNewFile())) {
+                DealLog.log(file, "写入到文件创建失败");
+                return;
+            }
 
-			// 构建FileOutputStream对象,文件不存在会自动新建--------------------------------
-			FileOutputStream fop = new FileOutputStream(file);
-			OutputStreamWriter outputStreamWriter = IoUtil.getUtf8Writer(fop);
-			outputStreamWriter.write(str);
-			// 关闭写入流,同时会把缓冲区内容写入文件,所以上面的注释掉
-			outputStreamWriter.close();
+            // 构建FileOutputStream对象,文件不存在会自动新建--------------------------------
+            FileOutputStream fop = new FileOutputStream(file);
+            OutputStreamWriter outputStreamWriter = IoUtil.getUtf8Writer(fop);
+            outputStreamWriter.write(str);
+            // 关闭写入流,同时会把缓冲区内容写入文件,所以上面的注释掉
+            outputStreamWriter.close();
 
-			// 关闭输出流,释放系统资源
-			fop.close();
-			DealLog.log(file, "完成");
-		} catch (IOException e) {
-			throw new TopException("写入到文件异常 路径", file, e);
-		}
-	}
+            // 关闭输出流,释放系统资源
+            fop.close();
+            DealLog.log(file, "完成");
+        } catch (IOException e) {
+            throw new TopException("写入到文件异常 路径", file, e);
+        }
+    }
 
-	/**
-	 * 读取文件内容到字符串
-	 *
-	 * @param path 文件路径
-	 * @return 文件字符串
-	 */
-	public static String getFileIo(File path) throws TopException {
-		StringBuilder sb = new StringBuilder();
-		//读取文件的内容
-		try (BufferedReader bufferedReader = new BufferedReader(
-				new InputStreamReader(Files.newInputStream(path.toPath()), StandardCharsets.UTF_8), 512 * 1024)
-		) {
-			String line;
+    /**
+     * 读取文件内容到字符串
+     *
+     * @param path 文件路径
+     * @return 文件字符串
+     */
+    public static String getFileIo(File path) throws TopException {
+        StringBuilder sb = new StringBuilder();
+        //读取文件的内容
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(Files.newInputStream(path.toPath()), StandardCharsets.UTF_8), 512 * 1024)) {
+            String line;
 
-			while ((line = bufferedReader.readLine()) != null) {
-				sb.append(line);
-				sb.append("\r\n");
-			}
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line);
+                sb.append("\r\n");
+            }
 
-			return sb.toString();
-		} catch (IOException e) {
-			throw new TopException("读取文件内容到字符串异常", e);
-		}
-	}
+            return sb.toString();
+        } catch (IOException e) {
+            throw new TopException("读取文件内容到字符串异常", e);
+        }
+    }
 
+    /**
+     * @param input 流
+     * @return byte数组
+     * @throws IOException 异常
+     */
+    public static byte[] toByteArray(InputStream input) throws IOException {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024 * 4];
+        int n;
+        while (-1 != (n = input.read(buffer))) {
+            output.write(buffer, 0, n);
+        }
+        return output.toByteArray();
+    }
+
+    /**
+     * @param filePath 文件路径
+     * @return 文件base64加密后的流
+     */
+    public static String fileToString(String filePath) {
+        File file = new File(filePath);
+        try (InputStream input = new FileInputStream(file)) {
+            byte[] buffer = toByteArray(input);
+            // 读到buffer字节数组中
+
+            return Base64.getEncoder().encodeToString(buffer);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    /**
+     * @param fileContent base64加密后的文件流
+     * @param filePath    文件写入位置
+     * @return 写入文件
+     */
+    public static boolean stringToFile(String fileContent, String filePath) {
+        byte[] bytes = Base64.getDecoder().decode(fileContent);
+        File file = new File(filePath).getParentFile();
+        if (!file.exists()) {
+            boolean s = file.mkdirs();
+            if (!s) {
+                return false;
+            }
+        }
+        try (InputStream input = new ByteArrayInputStream(bytes); OutputStream output = new FileOutputStream(filePath)) {
+            byte[] buf = new byte[1024 * 8];
+            int n;
+            while ((n = input.read(buf)) != -1) {
+                output.write(buf, 0, n);
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
--- reedtools/src/test/java/top/ireed/deal/DealIoTest.java
+++ reedtools/src/test/java/top/ireed/deal/DealIoTest.java
@@ -6,6 +6,9 @@ import top.ireed.general.TopException;
 
 import java.io.File;
 
+import static top.ireed.deal.DealIo.fileToString;
+import static top.ireed.deal.DealIo.stringToFile;
+
 /**
  * 功能简述:
  * 〈〉
@@ -16,8 +19,20 @@ import java.io.File;
  */
 public class DealIoTest extends TestCase {
 
-	public void testToFile() throws TopException {
-		DealIo.toFileIo(new File("D:\\cache\\io\\io测试数据.txt"), "测试内容111");
-		Assert.assertTrue(true);
-	}
+    public void testToFile() throws TopException {
+        DealIo.toFileIo(new File("D:\\cache\\io\\io测试数据.txt"), "测试内容111");
+        Assert.assertTrue(true);
+    }
+
+
+    public void testStringToFile() {
+
+        String filePath = "D:\\cache\\io\\ioFile测试数据.docx";
+
+        String fileContent = fileToString(filePath);
+
+        String outFilePath = "D:\\cache\\io\\" + System.currentTimeMillis() + File.separator + "ioFile测试数据.docx";
+        Boolean is = stringToFile(fileContent, outFilePath);
+
+    }
 }
