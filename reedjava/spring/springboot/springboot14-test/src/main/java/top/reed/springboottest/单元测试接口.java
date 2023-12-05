ef6e604272bf0e51b1f986acd9ae38e3f52fc207
reedsource <reedsource@189.cn>
2023-12-05 08:40:39 +0800

    springboot集成mock单元测试更新

---
+++ reedjava/spring/springboot/springboot14-test/src/main/java/top/reed/springboottest/单元测试接口.java
@@ -0,0 +1,8 @@
+package top.reed.springboottest;
+
+public interface 单元测试接口 {
+
+    String 有返回值接口(String msg);
+    void 无返回值接口(String msg);
+
+}
---
+++ reedjava/spring/springboot/springboot14-test/src/main/java/top/reed/springboottest/单元测试接口2.java
@@ -0,0 +1,8 @@
+package top.reed.springboottest;
+
+public interface 单元测试接口2 {
+
+    String 我是接口2有返回值接口(String msg);
+    void 我是接口2无返回值接口(String msg);
+
+}
