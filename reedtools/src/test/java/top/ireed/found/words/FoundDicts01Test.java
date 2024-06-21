43350a9805bd522f69aa300a0c57315887535b8b
reedsource <reedsource@189.cn>
2024-06-21 16:40:06 +0800

    更新 分词测试语句添加歧义测试语句

--- reedtools/src/test/java/top/ireed/found/words/FoundDicts01Test.java
+++ reedtools/src/test/java/top/ireed/found/words/FoundDicts01Test.java
@@ -9,7 +9,7 @@ public class FoundDicts01Test extends TestCase {
 
     public void testDict() {
         File file = new File(DealFile.getUserTestDataFile() + "\\分词\\词库.txt");
-        String msg = "分词的目标是将连续的汉字序列切分成有意义的词语，为后续的语义分析和信息处理提供基础。";
+        String msg = "分词的目标是将连续的汉字序列切分成有意义的词语，为后续的语义分析和信息处理提供基础。工作的和尚未工作的,羽毛球拍卖完了";
         System.out.println(msg);
         FoundDicts01 s = new FoundDicts01(msg, 3, file);
         System.out.println(s.go());
--- reedtools/src/test/java/top/ireed/found/words/FoundDicts02Test.java
+++ reedtools/src/test/java/top/ireed/found/words/FoundDicts02Test.java
@@ -8,7 +8,7 @@ import java.io.File;
 public class FoundDicts02Test extends TestCase {
     public void testDict() {
         File file = new File(DealFile.getUserTestDataFile() + "\\分词\\词库.txt");
-        String msg = "分词的目标是将连续的汉字序列切分成有意义的词语，为后续的语义分析和信息处理提供基础。";
+        String msg = "分词的目标是将连续的汉字序列切分成有意义的词语，为后续的语义分析和信息处理提供基础。工作的和尚未工作的,羽毛球拍卖完了";
         System.out.println(msg);
         FoundDicts02 s = new FoundDicts02(file);
         System.out.println(s.go(msg, 3));
