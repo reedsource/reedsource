b08d05cb21032d7ad6a14c65a2c0497d131ac48f
reedsource <reedsource@189.cn>
2024-06-24 18:52:03 +0800

    更新 合并分词结果时,添加一个评分计算

--- reedtools/src/main/java/top/ireed/found/words/FoundDicts02.java
+++ reedtools/src/main/java/top/ireed/found/words/FoundDicts02.java
@@ -2,10 +2,7 @@ package top.ireed.found.words;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 
 /**
@@ -23,6 +20,84 @@ public class FoundDicts02 {
         initDict(file);
     }
 
+    private static Map<String, List<Set<String>>> getStringListMap() {
+        Map<String, List<Set<String>>> map = new HashMap<>();
+        {
+            List<Set<String>> list = new ArrayList<>();
+            //前字数组
+            Set<String> lift = new HashSet<>() {{
+                add("");
+            }};
+            //后字数组
+            Set<String> right = new HashSet<>() {{
+                add("成");
+            }};
+            //前后-前数组
+            Set<String> liftOr = new HashSet<>() {{
+                add("");
+            }};
+            //前后-后数组
+            Set<String> rightOr = new HashSet<>() {{
+                add("");
+            }};
+            list.add(lift);
+            list.add(right);
+            list.add(liftOr);
+            list.add(rightOr);
+            map.put("切分", list);
+        }
+        {
+            List<Set<String>> list = new ArrayList<>();
+            //前字数组
+            Set<String> lift = new HashSet<>() {{
+                add("");
+            }};
+            //后字数组
+            Set<String> right = new HashSet<>() {{
+                add("意义");
+            }};
+            //前后-前数组
+            Set<String> liftOr = new HashSet<>() {{
+                add("");
+            }};
+            //前后-后数组
+            Set<String> rightOr = new HashSet<>() {{
+                add("");
+            }};
+            list.add(lift);
+            list.add(right);
+            list.add(liftOr);
+            list.add(rightOr);
+            map.put("有", list);
+        }
+        {
+            List<Set<String>> list = new ArrayList<>();
+            //前字数组
+            Set<String> lift = new HashSet<>() {{
+                add("");
+            }};
+            //后字数组
+            Set<String> right = new HashSet<>() {{
+                add("卖");
+            }};
+            //前后-前数组
+            Set<String> liftOr = new HashSet<>() {{
+                add("");
+            }};
+            //前后-后数组
+            Set<String> rightOr = new HashSet<>() {{
+                add("");
+            }};
+            list.add(lift);
+            list.add(right);
+            list.add(liftOr);
+            list.add(rightOr);
+            map.put("羽毛球拍", list);
+        }
+
+        return map;
+    }
+
     /**
      * 正反匹配组合
      *
@@ -35,15 +110,174 @@ public class FoundDicts02 {
         if (goMsg.equals(toMsg)) {
             return "双向匹配相同，结果为：" + goMsg;
         } else {
+
+            //词清单
+            Map<String, List<Set<String>>> map = getStringListMap();
+
+            //最终结果
+            StringBuilder result = new StringBuilder();
+
+            //正向分词集合
             List<String> goMsgList = Arrays.asList(goMsg.trim().split(" "));
+            //反向分词集合
             List<String> toMsgList = Arrays.asList(toMsg.trim().split(" "));
 
-            //取最小分组数量为最佳结果
-            if (goMsgList.size() >= toMsgList.size()) {
-                return "双向匹配不同，最佳结果为 反向：\r\n" + toMsg;
-            } else {
-                return "双向匹配不同，最佳结果为 正向：\r\n" + goMsg;
+            System.out.println("正向共 " + goMsgList.size());
+            System.out.println("反向共 " + toMsgList.size());
+
+            //正向总长度
+            int goSize = goMsgList.size();
+            //反向总长度
+            int toSize = toMsgList.size();
+
+            //分词位置游标
+            int is = 0;
+
+            //正确当前词
+            String dict;
+            //错误当前词
+            String word;
+            //下一个错误当前词
+            String leftWord;
+            //前后合并错误词
+            String allWord;
+            //游标小于正向分词集合长度  游标小于反向分词集合长度
+            while (is < goSize && is < toSize) {
+
+                //如果游标正反相等
+                if (goMsgList.get(is).equals(toMsgList.get(is))) {
+                    //写入最终分词结果
+                    result.append(goMsgList.get(is)).append(" ");
+                    is++;
+                } else {
+                    //正向当前词
+                    String leftDict = goMsgList.get(is);
+
+                    //正向匹配积分
+                    int leftSum = 0;
+
+                    {
+                        //左词清单
+                        List<Set<String>> list = map.get(leftDict);
+                        if (list != null && list.size() == 4) {
+                            //前字数组
+                            Set<String> lift = list.get(0);
+                            if (!lift.isEmpty()) {
+                                //当前词位置不是, 有上一个词的情况
+                                if (is > 0) {
+                                    //查询前词组是否存在
+                                    if (lift.contains(goMsgList.get(is - 1))) {
+                                        leftSum++;
+                                    }
+                                }
+                            }
+
+                            //后字数组
+                            Set<String> right = list.get(1);
+                            if (!right.isEmpty()) {
+                                //当前词位置不是, 有下一个词的情况
+                                //查询后词组是否存在
+                                if (right.contains(goMsgList.get(is + 1))) {
+                                    leftSum++;
+                                }
+                            }
+                            //前后-前数组
+                            Set<String> liftOr = list.get(2);
+                            //前后-后数组
+                            Set<String> rightOr = list.get(3);
+                            if (!liftOr.isEmpty() && !rightOr.isEmpty()) {
+                                //当前词位置不是, 有上一个词的情况
+                                if (is > 0) {
+                                    //查询前词组是否存在
+                                    if (liftOr.contains(goMsgList.get(is + 1)) && rightOr.contains(goMsgList.get(is + 1))) {
+                                        leftSum++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    //反向当前词
+                    String rightDict = toMsgList.get(is);
+                    //正向匹配积分
+                    int rightSum = 0;
+
+                    {
+                        //左词清单
+                        List<Set<String>> list = map.get(rightDict);
+                        if (list != null && list.size() == 4) {
+                            //前字数组
+                            Set<String> lift = list.get(0);
+                            if (!lift.isEmpty()) {
+                                //当前词位置不是, 有上一个词的情况
+                                if (is > 0) {
+                                    //查询前词组是否存在
+                                    if (lift.contains(toMsgList.get(is - 1))) {
+                                        rightSum++;
+                                    }
+                                }
+                            }
+
+                            //后字数组
+                            Set<String> right = list.get(1);
+                            if (!right.isEmpty()) {
+                                //当前词位置不是, 有下一个词的情况
+                                //查询后词组是否存在
+                                if (right.contains(toMsgList.get(is + 1))) {
+                                    rightSum++;
+                                }
+                            }
+                            //前后-前数组
+                            Set<String> liftOr = list.get(2);
+                            //前后-后数组
+                            Set<String> rightOr = list.get(3);
+                            if (!liftOr.isEmpty() && !rightOr.isEmpty()) {
+                                //当前词位置不是, 有上一个词的情况
+                                if (is > 0) {
+                                    //查询前词组是否存在
+                                    if (liftOr.contains(toMsgList.get(is + 1)) && rightOr.contains(toMsgList.get(is + 1))) {
+                                        rightSum++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    //正向大于反向
+                    //反向包含等于的情况, 即结果偏向
+                    if (leftSum > rightSum) {
+                        //正向当前词
+                        dict = goMsgList.get(is);
+                        //更新反向分词结果
+                        //反向当前词
+                        word = toMsgList.get(is);
+                        //获取反向下一个词
+                        leftWord = toMsgList.get(is + 1);
+                        //合并词语
+                        allWord = word + leftWord;
+
+                        //更新反向当前词
+                        toMsgList.set(is, dict);
+                        //更新反向下一词
+                        toMsgList.set(is + 1, allWord.substring(dict.length()));
+                    } else {
+                        //反向当前词
+                        dict = toMsgList.get(is);
+
+                        //更新正向分词结果
+                        word = goMsgList.get(is);
+                        //获取反向下一个词
+                        leftWord = goMsgList.get(is + 1);
+                        //合并词语
+                        allWord = word + leftWord;
+
+                        //更新反向当前词
+                        goMsgList.set(is, dict);
+                        goMsgList.set(is + 1, allWord.substring(dict.length()));
+                    }
+                }
             }
+
+            return result.toString();
         }
     }
 
--- reedtools/src/test/java/top/ireed/found/words/FoundDicts02Test.java
+++ reedtools/src/test/java/top/ireed/found/words/FoundDicts02Test.java
@@ -11,9 +11,9 @@ public class FoundDicts02Test extends TestCase {
         String msg = "分词的目标是将连续的汉字序列切分成有意义的词语，为后续的语义分析和信息处理提供基础。工作的和尚未工作的,羽毛球拍卖完了";
         System.out.println(msg);
         FoundDicts02 s = new FoundDicts02(file);
-        System.out.println(s.go(msg, 3));
-        System.out.println(s.to(msg, 3));
-        System.out.println(s.goTo(msg, 3));
+        System.out.println(s.go(msg, 10));
+        System.out.println(s.to(msg, 10));
+        System.out.println(s.goTo(msg, 10));
     }
 
 }
